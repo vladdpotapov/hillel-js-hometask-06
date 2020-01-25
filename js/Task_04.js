e1fba7858e69dc818d9f354b54f792eeed650afd
PerSick <vlad461@ukr.net>
2020-01-25 22:08:14 +0300

    Task #04 - done

--- js/Task_04.js
+++ js/Task_04.js
@@ -1,3 +1,20 @@
 // TASK #04:
 // Написать функцию заполнения пользовательскими данными двумерного массива. 
-// Длину массива и внутренних массивов задаёт пользователь
+// Длину массива и внутренних массивов задаёт пользователь.
+
+let array = [];
+
+function fillMultidimensionalArray() {
+    let subArraysQuantity = parseInt(prompt('Enter the quantity of sub arrays'));
+    for (let i = 0; i < subArraysQuantity; i++) {
+        let subArraysElementsQuantity = prompt(`Enter the sub array ${i + 1} elements quantity`);
+        array[i] = [];
+
+        for (let j = 0; j < subArraysElementsQuantity; j++) {
+            array[i].push(prompt('Add the element'));
+        }
+    }
+    console.log(array);
+}
+
+fillMultidimensionalArray();
